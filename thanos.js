ecbe34a644bdd37c664ea8348eb707d45718f8b8
bigtimes <nou@420blaze.it>
2019-10-12 20:37:17 -0400

    New feature, targeted chat rooms!

--- thanos.js
+++ thanos.js
@@ -9,21 +9,59 @@ client.on('ready', () => {
 
 /* Basically handles parsing */
 client.on('message', message => {
-    if (message.content === `${prefix}snap`) {
-	try {
-	    const VCmembers       = message.member.voiceChannel.members;
-	    const sizeBalanced    = Math.floor(Array.from(VCmembers).length / 2);
-	    const balancedMembers = VCmembers.random(sizeBalanced);
-	
-	    message.channel.send("Equally balanced, as all things should be.");
-	    for (var i = 0; i < sizeBalanced; ++i)
-		balancedMembers[i].setVoiceChannel(null);
-	}
-	catch (e) {
-	    if (e instanceof RangeError)
-		console.log("Too few members in voice channel.");
-	    if (e instanceof TypeError)
-		console.log("Property 'members' undefined, perhaps no one is in channel.");
+
+    /* Make sure it's a command */
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args    = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+    const guild   = message.guild;
+
+    if (command === 'snap') {
+	if (!args.length) {
+	    try {
+		const VCmembers       = message.member.voiceChannel.members;
+		const sizeBalanced    = Math.floor(Array.from(VCmembers).length / 2);
+		const balancedMembers = VCmembers.random(sizeBalanced);
+
+		message.channel.send("Equally balanced, as all things should be.");
+		for (var i = 0; i < sizeBalanced; ++i)
+		    balancedMembers[i].setVoiceChannel(null);
+	    }
+	    catch (e) {
+		if (e instanceof RangeError)
+		    console.log("Too few members in voice channel.");
+		if (e instanceof TypeError)
+		    console.log("Property 'members' undefined, perhaps no one is in channel.");
+	    }
+	} else {
+	    let channel;
+	    const channelName = args.join(" ");
+	    if (!guild.available) return;
+
+	    const guildArrays = guild.channels.array();
+	    for (i in guildArrays) {
+		if (guildArrays[i].name === channelName) {
+		    channel = guildArrays[i];
+		    break;
+		}
+	    }
+
+	    try {
+		const VCmembers       = channel.members;
+		const sizeBalanced    = Math.floor(Array.from(VCmembers).length / 2);
+		const balancedMembers = VCmembers.random(sizeBalanced);
+
+		message.channel.send("Equally balanced, as all things should be.");
+		for (var i = 0; i < sizeBalanced; ++i)
+		    balancedMembers[i].setVoiceChannel(null);
+	    }
+	    catch (e) {
+		if (e instanceof RangeError)
+		    console.log("Too few members in voice channel.");
+		if (e instanceof TypeError)
+		    console.log("Property 'members' undefined, perhaps no one is in channel.");
+	    }
 	}
     }
 });
