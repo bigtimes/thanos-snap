75e1ef1643e975aed9eb72ac8220fb29771a75d5
bigtimes <nou@420blaze.it>
2020-07-18 12:20:52 -0400

    Formatting

--- thanos.js
+++ thanos.js
@@ -6,76 +6,77 @@ const client  = new Discord.Client();
 // For snapping
 function snap(message, channel = null) 
 {
-	try 
-	{
-		let VCMembers;
-		if (channel == null)
-			VCMembers = message.member.voice.channel.members;
-		else
-			VCmembers = channel.members;
+    try 
+    {
+	let VCMembers;
+	if (channel == null)
+	    VCMembers = message.member.voice.channel.members;
+	else
+	    VCmembers = channel.members;
 		
-		const sizeBalanced    = Math.floor(Array.from(VCMembers).length / 2);
-		const balancedMembers = VCMembers.random(sizeBalanced);
+	const sizeBalanced    = Math.floor(Array.from(VCMembers).length / 2);
+	const balancedMembers = VCMembers.random(sizeBalanced);
 
-		let VoiceStates = new Array();
-		for (let gm of balancedMembers.values()) {
-			VoiceStates.push(gm.voice);
-		}
-
-		message.channel.send("Equally balanced, as all things should be.");
-
-		console.log(VoiceStates);
-		for (let i = 0; i < sizeBalanced; ++i) {
-			VoiceStates[i].setChannel(null);
-		}
-	} catch(e) 
-	{
-		// Not enough members
-		if (e instanceof RangeError)
-			console.log("Too few members in voice channel.");
-		// No one is in the channel
-		if (e instanceof TypeError)
-		console.log("Property 'members' undefined, perhaps no one is in channel.");
+	let VoiceStates = new Array();
+	for (let gm of balancedMembers.values()) {
+	    VoiceStates.push(gm.voice);
 	}
+
+	message.channel.send("Equally balanced, as all things should be.");
+
+	console.log(VoiceStates);
+	for (let i = 0; i < sizeBalanced; ++i) {
+	    VoiceStates[i].setChannel(null);
+	}
+    } catch(e) 
+    {
+        // Not enough members
+	if (e instanceof RangeError)
+	    console.log("Too few members in voice channel.");
+	// No one is in the channel
+	if (e instanceof TypeError)
+	    console.log("Property 'members' undefined, perhaps no one is in channel.");
+    }
 }
 
 // Start the bot
 client.on('ready', () => 
 {
-	console.log('Ready...');
-	console.log('Prefix:' + prefix);
+    console.log('Ready...');
+    console.log('Prefix:' + prefix);
 });
 
 // Handles parsing of commands
 client.on('message', message => 
 {
-	// If prefix not found, or messager is a bot
-    	if (!message.content.startsWith(prefix) || message.author.bot) return;
+    // If prefix not found, or messager is a bot
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    	const args    = message.content.slice(prefix.length).toLowerCase().split(" "); // Get the pure argument
-    	const guild   = message.guild; // Get the guild of the message
-	console.log(args);
+    // Get the pure argument
+    const args    = message.content.slice(prefix.length).toLowerCase().split(" "); 
+    // Get the guild of the message
+    const guild   = message.guild; 
+
+    switch(args[0]) {
+    case "snap": 
+	if (args.length < 2)
+	   snap(message); // Snapping...
+	else {
+	    let channel = args[1];
+	    if (!guild.available) return;
 	
-	switch(args[0]) {
-	case "snap": 
-		if (args.length < 2)
-			snap(message); // Snapping...
-		else {
-			let channel = args[1];
-			if (!guild.available) return;
+	    const channels = guild.channels.cache;
+	    for (let i of guildArrays) {
+		if (guildArrays[i].name === channelName) {
+		    channel = guildArrays[i];
+			break;
+		}
+    	    }
 	
-			const channels = guild.channels.cache;
-			for (let i of guildArrays) {
-				if (guildArrays[i].name === channelName) {
-					channel = guildArrays[i];
-					break;
-				}
-    			}
-	
-			// Snapping...
-			snap(message, channel);
-	    	}
+	    // Snapping...
+	    snap(message, channel);
 	}
+    }
 });
 
 client.login(token);
