399a8cfa64bf4c4bd14ada6761cd6ec9459ecf66
bigtimes <nou@420blaze.it>
2020-07-18 12:06:36 -0400

    API update

--- thanos.js
+++ thanos.js
@@ -4,21 +4,32 @@ const Discord = require('discord.js');
 const client  = new Discord.Client();
 
 // For snapping
-function snap(message, channel) {
-	try {
+function snap(message, channel = null) 
+{
+	try 
+	{
+		let VCMembers;
 		if (channel == null)
-			const VCMembers = message.member.voiceChannel.members;
+			VCMembers = message.member.voice.channel.members;
 		else
-			const VCmembers = channel.members;
-
+			VCmembers = channel.members;
+		
 		const sizeBalanced    = Math.floor(Array.from(VCMembers).length / 2);
 		const balancedMembers = VCMembers.random(sizeBalanced);
 
+		let VoiceStates = new Array();
+		for (let gm of balancedMembers.values()) {
+			VoiceStates.push(gm.voice);
+		}
+
 		message.channel.send("Equally balanced, as all things should be.");
 
-		for (let i = 0; i < sizeBalanced; ++i)
-			balanceMembers[i].setVoiceChannel(null);
-	} catch(e) {
+		console.log(VoiceStates);
+		for (let i = 0; i < sizeBalanced; ++i) {
+			VoiceStates[i].setChannel(null);
+		}
+	} catch(e) 
+	{
 		// Not enough members
 		if (e instanceof RangeError)
 			console.log("Too few members in voice channel.");
@@ -29,38 +40,40 @@ function snap(message, channel) {
 }
 
 // Start the bot
-client.on('ready', () => {
-    console.log('Ready...');
+client.on('ready', () => 
+{
+	console.log('Ready...');
+	console.log('Prefix:' + prefix);
 });
 
 // Handles parsing of commands
-client.on('message', message => {
+client.on('message', message => 
+{
 	// If prefix not found, or messager is a bot
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args    = message.content.slice(prefix.length).split(' '); // Get the pure argument
-    const command = args.shift().toLowerCase(); // Easier parsing, this also changes args
-    const guild   = message.guild; // Get the guild of the message
-
-	switch(args) {
-		case "snap": 
-			if (!args.length)
-				snap(message); // Snapping...
-			else {
-				let channel;
-				const channelName = args.join(" ");
-				if (!guild.available) return;
-
-				const guildArrays = guild.channels.array();
-				for (i in guildArrays) {
-					if (guildArrays[i].name === channelName) {
-						channel = guildArrays[i];
-						break;
-					}
-	    		}
-
-				// Snapping...
-				snap(message, channel);
+    	const args    = message.content.slice(prefix.length).toLowerCase().split(" "); // Get the pure argument
+    	const guild   = message.guild; // Get the guild of the message
+	console.log(args);
+	
+	switch(args[0]) {
+	case "snap": 
+		if (args.length < 2)
+			snap(message); // Snapping...
+		else {
+			let channel = args[1];
+			if (!guild.available) return;
+	
+			const channels = guild.channels.cache;
+			for (let i of guildArrays) {
+				if (guildArrays[i].name === channelName) {
+					channel = guildArrays[i];
+					break;
+				}
+    			}
+	
+			// Snapping...
+			snap(message, channel);
 	    	}
 	}
 });
